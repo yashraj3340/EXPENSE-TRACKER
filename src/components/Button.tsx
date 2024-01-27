1ae9cc705c72e8b56b69c2e304cd2bfb5989fd94
yashraj3340 <bt22csh012@iiitn.ac.in>
2024-01-27 12:30:04 +0530

    initial commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface Props {
+  children: string;
+  onClick: () => void;
+  color?:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "danger"
+    | "warning"
+    | "info"
+    | "light"
+    | "dark";
+}
+
+const Button = ({ children, onClick, color = "primary" }: Props) => {
+  return (
+    <button className={"btn btn-" + color} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/components/ListGroup/ListGroup.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+interface ListItemProps {
+  active: boolean;
+}
+const ListItem = styled.li<ListItemProps>`
+  padding: 5px 0;
+  background: ${(props) => (props.active ? "blue" : "transparent")};
+`;
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>There are no items in the list</p>}
+      <List>
+        {items.map((item, index) => (
+          <ListItem
+            active={index === selectedIndex}
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+}
+
+export default ListGroup;
